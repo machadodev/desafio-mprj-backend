0d3e5d1ed1b931bbae862c6ec060365dddf4ff2f
L. Machado <leonardomachado@tic.ufrj.br>
2021-07-12 13:54:22 -0300

    validação da rota somente no serializer

--- app/desafio/urls.py
+++ app/desafio/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from desafio import views
 
 urlpatterns = [
-    path("<int:id_documento>", views.TramitacaoDocumentoAPIView.as_view(),name="tramitacao_documento"),
+    path("<id_documento>", views.TramitacaoDocumentoAPIView.as_view(),name="tramitacao_documento"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
