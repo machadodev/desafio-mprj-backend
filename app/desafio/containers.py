c1809f11dc7c727dc1a0fe55fac0a1919dd22977
L. Machado <leonardomachado@tic.ufrj.br>
2021-07-13 15:34:55 -0300

    Kafka singleton para conectar somente uma vez

--- app/desafio/containers.py
+++ app/desafio/containers.py
@@ -8,5 +8,5 @@ class Container(containers.DeclarativeContainer):
     config = providers.Configuration()
     cache_service : CacheService = providers.Factory(RedisCacheService, int(os.environ.get("CACHE_TTL_IN_SECONDS", default=10)))    
     tjrj_service : TJRJService = providers.Factory(MockTJRJSOAPService, os.environ.get("ACCESS_TOKEN_TJRJ", default=''))
-    log_service : LogService = providers.Factory(KafkaLoggerService)
+    log_service : LogService = providers.Singleton(KafkaLoggerService)
     database_service : DatabaseService = providers.Factory(MockExternalDatabaseService)
