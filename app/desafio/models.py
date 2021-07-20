07106f90355aa9a99f0f611812dc80df6ddaf551
Leonardo Machado <leonardoms@id.uff.br>
2021-07-19 23:49:11 -0300

    criada chave primaria explicitamente nos models

--- app/desafio/models.py
+++ app/desafio/models.py
@@ -5,11 +5,13 @@ from django.contrib.auth.models import User
 # Caso precise criar algum modelo para sua aplicação
 
 class Documento(models.Model):
+    id = models.AutoField(primary_key=True)
     id_doc = models.IntegerField()
     id_tjrj = models.IntegerField()
     hash_tramitacao = models.CharField(max_length=32)
 
 
 class UsuarioObservers(models.Model):
+    id = models.AutoField(primary_key=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     id_tjrj = models.IntegerField()
